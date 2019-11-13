94c51680dc953104549304c59ccc834e1b64014c
Samarth Khatwani <samarthkhatwani47@gmail.com>
2019-11-12 23:24:56 -0800

    health check grading microservice

---
+++ backend/GradingMicroservice/src/GradingMS/go/controller/health-check-route.go
@@ -0,0 +1,14 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/unrolled/render"
+)
+
+//PingHandler returns a Handler for Ping Request
+func PingHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		formatter.JSON(w, http.StatusOK, struct{ Message string }{"Grading API is up and running !!"})
+	}
+}
--- backend/GradingMicroservice/src/GradingMS/main.go
+++ backend/GradingMicroservice/src/GradingMS/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	ctrl "GradingMS/go/controller"
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-
+	log.Printf("Server started")
+	router := ctrl.NewRouter()
 	fmt.Println("Grading Microservice main method")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
