208f2e5de258e71edaa6a17bfe2c4df8ed85eefe
hariae <aehari2010@gmail.com>
2019-11-13 22:42:56 -0800

    Chnages to Enrollment Cart

--- frontend/student-portal/src/components/Enrollment/EnrollmentCart.js
+++ frontend/student-portal/src/components/Enrollment/EnrollmentCart.js
@@ -2,7 +2,54 @@ import React, {Component} from 'react';
 
 class EnrollmentCart extends Component {
     render() {
-        <h1> EnrollmentCart </h1>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Class</th>
+      <th scope="col">Time</th>
+      <th scope="col">Instructor</th>
+      <th scope="col">Credits</th>
+      <th scope="col">Status</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>      
+      <td>CMPE219-01</td>
+      <td>Th 6:00PM - 8:45PM</td>
+      <td>@A Moolam</td>
+      <td>3.0</td>
+      <td>Enrolled</td>
+    </tr>
+    <tr>      
+      <td>CMPE219-01</td>
+      <td>Th 6:00PM - 8:45PM</td>
+      <td>@A Moolam</td>
+      <td>3.0</td>
+      <td>Enrolled</td>
+    </tr>
+    <tr>      
+      <td>CMPE219-01</td>
+      <td>Th 6:00PM - 8:45PM</td>
+      <td>@A Moolam</td>
+      <td>3.0</td>
+      <td>Enrolled</td>
+    </tr>
+    <tr>      
+      <td>CMPE219-01</td>
+      <td>Th 6:00PM - 8:45PM</td>
+      <td>@A Moolam</td>
+      <td>3.0</td>
+      <td>Enrolled</td>
+    </tr>
+    <tr>      
+      <td>CMPE219-01</td>
+      <td>Th 6:00PM - 8:45PM</td>
+      <td>@A Moolam</td>
+      <td>3.0</td>
+      <td>Enrolled</td>
+    </tr>
+  </tbody>
+</table>
     }
 }
 
