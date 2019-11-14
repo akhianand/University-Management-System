aba913bf235da547cd941c4a20ec3dfcfa8a55bd
hariae <aehari2010@gmail.com>
2019-11-13 22:39:32 -0800

    Changes to Enrollment

---
+++ frontend/student-portal/src/components/Enrollment/Enrollment.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Enrollment extends Component {
+    render() {
+        return (
+            <h1> Enrollment </h1>
+        )
+    }
+}
+
+export default Enrollment;
---
+++ frontend/student-portal/src/components/Enrollment/EnrollmentCart.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class EnrollmentCart extends Component {
+    render() {
+        <h1> EnrollmentCart </h1>
+    }
+}
+
+export default EnrollmentCart;
