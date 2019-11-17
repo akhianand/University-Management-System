bf76beae5817132b886ba1ce7639691c4ae8b207
Samarth Khatwani <samarthkhatwani47@gmail.com>
2019-11-16 19:47:35 -0800

    Refactor - Removing CRUD operations from grade struct

--- backend/GradingMicroservice/src/GradingMS/go/model/grade.go
+++ backend/GradingMicroservice/src/GradingMS/go/model/grade.go
@@ -1,13 +1,5 @@
 package model
 
-import (
-	util "GradingMS/go/util"
-	"log"
-	"os"
-
-	"gopkg.in/mgo.v2"
-)
-
 // Grade encapsulates a grade object
 type Grade struct {
 	StudentID    int    `bson:"studentid"`
@@ -15,40 +7,4 @@ type Grade struct {
 	Term         string `bson:"term"`
 	Grade        string `bson:"grade"`
 	InstructorID string `bson:"instructorid"`
-}
-
-// Insert inserts the grade document to mongo db
-func (g *Grade) Insert() {
-	log.Printf("Insert Grade dao method")
-	session, err := mgo.Dial(os.Getenv("MONGO_URL"))
-	if err != nil {
-		//this will crash the server
-		util.FailOnError(err, "Mongo Dial Error")
-	}
-	defer session.Close()
-	session.SetMode(mgo.Monotonic, true)
-	c := session.DB(os.Getenv("DATABASE")).C("grade")
-	if err := c.Insert(g); err != nil {
-		//this will crash the server
-		util.FailOnError(err, "Mongo Insert Error")
-	}
-}
-
-//Find ff
-func (g *Grade) Find(gradeQueryFilter *GradeQueryFilter) ([]Grade, error) {
-	log.Printf("Get Grade dao method")
-	session, err := mgo.Dial(os.Getenv("MONGO_URL"))
-	if err != nil {
-		//this will crash the server
-		util.FailOnError(err, "Mongo Dial Error")
-	}
-	defer session.Close()
-	session.SetMode(mgo.Monotonic, true)
-	c := session.DB(os.Getenv("DATABASE")).C("grade")
-	var grades []Grade = []Grade{}
-	err = c.Find(gradeQueryFilter).All(&grades)
-	if err != nil {
-		return nil, util.NewInternalServerError("Mongo find Error " + err.Error())
-	}
-	return grades, nil
 }
