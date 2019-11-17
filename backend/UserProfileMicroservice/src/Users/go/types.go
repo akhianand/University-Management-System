8e8da581b1a39b06b9ba64711c9c77123cd2d0d5
Akhilesh Anand <akhilesh.anand@sjsu.edu>
2019-11-17 13:23:18 -0800

    Address Type Added

--- backend/UserProfileMicroservice/src/Users/go/types.go
+++ backend/UserProfileMicroservice/src/Users/go/types.go
@@ -17,21 +17,32 @@ type Route struct {
 // Routes slice
 type Routes []Route
 
+// Address Structure
+type Address struct {
+	AddressLine1 string `bson:"addressline1"`
+	AddressLine2 string `bson:"addressline2"`
+	City         string `bson:"city"`
+	State        string `bson:"state"`
+	Zip          string `bson:"zip"`
+}
+
 // User Structure
 type User struct {
-	UserID    int
-	Firstname string
-	Lastname  string
-	Role      string
-	Email     string
-	Password  string
-	Image     string
+	UserID     int     `bson:"userid"`
+	Firstname  string  `bson:"firstname"`
+	Lastname   string  `bson:"lastname"`
+	Role       string  `bson:"role"`
+	Email      string  `bson:"email"`
+	Password   string  `bson:"password"`
+	Image      string  `bson:"image"`
+	Address    Address `bson:"address"`
+	Department string  `bson:"department"`
 }
 
 // Login Structure
 type Login struct {
-	Email    string
-	Password string
+	Email    string `bson:"email"`
+	Password string `bson:"password"`
 }
 
 //FormatHandlerFunc returns a handler function formatted as passed in argument
