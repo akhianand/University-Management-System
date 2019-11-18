d1bb124aadabdd541d5e47ea755d72a31cac3c46
Akhilesh Anand <akhilesh.anand@sjsu.edu>
2019-11-17 22:57:08 -0800

    Signup Updates

--- frontend/student-portal/src/components/Profile/Login.js
+++ frontend/student-portal/src/components/Profile/Login.js
@@ -2,6 +2,47 @@ import React, { Component } from "react";
 import LHeader from "./LHeader";
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      isAdmin: false,
+      isStudent: false,
+	  hasError: false,
+	  errorMessage: ""
+    };
+
+    this.submitLogin = this.submitLogin.bind(this);
+    this.emailChangeHandler = this.emailChangeHandler.bind(this);
+    this.passwordChangeHandler = this.passwordChangeHandler.bind(this);
+  }
+
+  submitLogin = e => {
+    e.preventDefault();
+    const login = {
+      email: this.state.email,
+      password: this.state.password
+    };
+	console.log(login);
+	
+	this.setState({
+		hasError: true,
+		errorMessage: "Error, Verify Password"
+	})
+  };
+
+  emailChangeHandler = e => {
+    this.setState({
+      email: e.target.value
+    });
+  };
+  passwordChangeHandler = e => {
+    this.setState({
+      password: e.target.value
+    });
+  };
+
   render() {
     return (
       <>
@@ -10,10 +51,19 @@ class Login extends Component {
         <div class="container py-5">
           <div class="row text-center">
             <div class="col-md-6 mx-auto">
-              <h1 style={{fontWeight:"lighter"}}>Login</h1>
+              <h1 style={{ fontWeight: "lighter" }}>Login</h1>
             </div>
           </div>
-		  <br/>
+          <br />
+        
+		{ this.state.hasError ?  <div class="row text-center">
+            <div class="col-md-6 mx-auto">
+              <div class="alert alert-danger" role="alert">
+                {this.state.errorMessage}
+              </div>
+            </div>
+	</div> : null}
+
           <div class="row">
             <div class="col-md-12">
               <div class="row">
@@ -29,6 +79,7 @@ class Login extends Component {
                             id="email"
                             placeholder="john.doe@sjsu.edu"
                             required="true"
+                            onChange={this.emailChangeHandler}
                           />
                         </div>
                         <div class="form-group">
@@ -39,14 +90,14 @@ class Login extends Component {
                             id="password"
                             placeholder="Password"
                             required="true"
+                            onChange={this.passwordChangeHandler}
                           />
                         </div>
                         <br />
                         <div class="form-group">
                           <button
-                            type="submit"
-							class="btn btn-primary btn-lg btn-block"
-							
+                            onClick={this.submitLogin}
+                            class="btn btn-primary btn-lg btn-block"
                           >
                             Login
                           </button>
--- frontend/student-portal/src/components/Profile/Signup.js
+++ frontend/student-portal/src/components/Profile/Signup.js
@@ -1,103 +1,224 @@
 import React, { Component } from "react";
 import LHeader from "./LHeader";
-
+import axios from "axios";
+import { API_URL, API_URL } from "./config.js";
 
 class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstname: "",
+      lastname: "",
+      department: "student",
+      email: "",
+      password: "",
+      verifypassword: "",
+      hasError: "",
+      errorMessage: "",
+      userLoggedIn: ""
+    };
+
+    this.submitSignup = this.submitSignup.bind(this);
+    this.emailChangeHandler = this.emailChangeHandler.bind(this);
+    this.passwordChangeHandler = this.passwordChangeHandler.bind(this);
+    this.departmentChangeHandler = this.departmentChangeHandler.bind(this);
+    this.firstnameChangeHandler = this.firstnameChangeHandler.bind(this);
+    this.lastnameChangeHandler = this.lastnameChangeHandler.bind(this);
+    this.verifyPasswordChangeHandler = this.verifyPasswordChangeHandler.bind(
+      this
+    );
+  }
+
+  submitSignup = e => {
+    e.preventDefault();
+    const signup = {
+      firstname: this.state.firstname,
+      lastname: this.state.lastname,
+      email: this.state.email,
+      password: this.state.password,
+      department: this.state.department
+    };
+    console.log(signup);
+
+    // axios.defaults.withCredentials = true;
+    // axios
+    //   .post(`${API_URL}:${API_PORT}/signup`, signup)
+    //   .then(response => {
+    //     if (response.status === 200) {
+    //       if (response.data.success) {
+    //         this.setState({
+    //           userLoggedIn: true,
+    //           hasError: false,
+    //           userErrorMessage: ""
+    //         });
+    //       } else {
+    //         this.setState({
+    //           userLoggedIn: false,
+    //           hasError: true,
+    //           userErrorMessage: response.data.error
+    //         });
+    //       }
+    //     } else {
+    //       this.setState({
+    //         userLoggedIn: false,
+    //         hasError: true,
+    //         userErrorMessage: response.data.err
+    //       });
+    //     }
+    //   })
+    //   .catch(err => {
+    //     this.setState({
+    //       userLoggedIn: false,
+    //       hasError: true,
+    //       userErrorMessage: "An Error Has Occoured!"
+    //     });
+    //   });
+  };
+
+  firstnameChangeHandler = e => {
+    this.setState({
+      firstname: e.target.value
+    });
+  };
+  lastnameChangeHandler = e => {
+    this.setState({
+      lastname: e.target.value
+    });
+  };
+  departmentChangeHandler = e => {
+    this.setState({
+      department: e.target.value
+    });
+  };
+  emailChangeHandler = e => {
+    this.setState({
+      email: e.target.value
+    });
+  };
+  passwordChangeHandler = e => {
+    this.setState({
+      password: e.target.value
+    });
+  };
+
+  verifyPasswordChangeHandler = e => {
+    this.setState({
+      verifypassword: e.target.value
+    });
+  };
+
   render() {
     return (
-		<>
+      <>
         <LHeader />
 
-      <div class="container py-5">
-		   <div class="row text-center">
+        <div class="container py-5">
+          <div class="row text-center">
             <div class="col-md-6 mx-auto">
-              <h1 style={{fontWeight:"lighter"}}>SignUp</h1>
+              <h1 style={{ fontWeight: "lighter" }}>SignUp</h1>
             </div>
           </div>
-		  <br/>
-        <div class="row">
-          <div class="col-md-12">
-            <div class="row">
+          <br />
+          {this.state.hasError ? (
+            <div class="row text-center">
               <div class="col-md-6 mx-auto">
-                <div class="card border-secondary">
-                
-                  <div class="card-body">
-                    <form class="form" role="form" autocomplete="off">
-                      <div class="form-group">
-                        <label for="inputName">First Name</label>
-                        <input
-                          type="text"
-                          class="form-control"
-                          id="firstname"
-                          placeholder="First Name"
-                          required="true"
-                        />
-                      </div>
-                      <div class="form-group">
-                        <label for="inputName">Last Name</label>
-                        <input
-                          type="text"
-                          class="form-control"
-                          id="lastname"
-                          placeholder="Last Name"
-                          required="true"
-                        />
-                      </div>
-                      <div class="form-group">
-                        <label for="email">Email</label>
-                        <input
-                          type="email"
-                          class="form-control"
-                          id="email"
-                          placeholder="john.doe@sjsu.edu"
-                          required="true"
-                        />
-                      </div>
-                      <div class="form-group">
-                        <label for="department">Department</label>
-                        <select class="form-control" id="department">
-                          <option>Student</option>
-                          <option>Faculty</option>
-                        </select>
-                      </div>
-                      <div class="form-group">
-                        <label for="password">Password</label>
-                        <input
-                          type="password"
-                          class="form-control"
-                          id="password"
-                          placeholder="Password"
-                          required="true"
-                        />
-                      </div>
-                      <div class="form-group">
-                        <label for="password">Verify Password</label>
-                        <input
-                          type="password"
-                          class="form-control"
-                          id="password"
-                          placeholder="Verify Password"
-                          required="true"
-                        />
-                      </div>
-					  <br/>
-                      <div class="form-group">
-                        <button
-                          type="submit"
-                          class="btn btn-primary btn-lg btn-block"
-                        >
-                          Register
-                        </button>
-                      </div>
-                    </form>
+                <div class="alert alert-danger" role="alert">
+                  {this.state.errorMessage}
+                </div>
+              </div>
+            </div>
+          ) : null}
+          <div class="row">
+            <div class="col-md-12">
+              <div class="row">
+                <div class="col-md-6 mx-auto">
+                  <div class="card border-secondary">
+                    <div class="card-body">
+                      <form class="form" role="form" autocomplete="off">
+                        <div class="form-group">
+                          <label for="inputName">First Name</label>
+                          <input
+                            type="text"
+                            class="form-control"
+                            id="firstname"
+                            placeholder="First Name"
+                            required="true"
+                            onChange={this.firstnameChangeHandler}
+                          />
+                        </div>
+                        <div class="form-group">
+                          <label for="inputName">Last Name</label>
+                          <input
+                            type="text"
+                            class="form-control"
+                            id="lastname"
+                            placeholder="Last Name"
+                            required="true"
+                            onChange={this.lastnameChangeHandler}
+                          />
+                        </div>
+                        <div class="form-group">
+                          <label for="email">Email</label>
+                          <input
+                            type="email"
+                            class="form-control"
+                            id="email"
+                            placeholder="john.doe@sjsu.edu"
+                            required="true"
+                            onChange={this.emailChangeHandler}
+                          />
+                        </div>
+                        <div class="form-group">
+                          <label for="department">Department</label>
+                          <select
+                            onChange={this.departmentChangeHandler}
+                            class="form-control"
+                            id="department"
+                          >
+                            <option value="student">Student</option>
+                            <option value="faculty">Faculty</option>
+                          </select>
+                        </div>
+                        <div class="form-group">
+                          <label for="password">Password</label>
+                          <input
+                            type="password"
+                            class="form-control"
+                            id="password"
+                            placeholder="Password"
+                            onChange={this.passwordChangeHandler}
+                            required="true"
+                          />
+                        </div>
+                        <div class="form-group">
+                          <label for="password">Verify Password</label>
+                          <input
+                            type="password"
+                            class="form-control"
+                            id="password"
+                            onChange={this.verifyPasswordChangeHandler}
+                            placeholder="Verify Password"
+                            required="true"
+                          />
+                        </div>
+                        <br />
+                        <div class="form-group">
+                          <button
+                            onClick={this.submitSignup}
+                            class="btn btn-primary btn-lg btn-block"
+                          >
+                            Register
+                          </button>
+                        </div>
+                      </form>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-	  </>
+      </>
     );
   }
 }
