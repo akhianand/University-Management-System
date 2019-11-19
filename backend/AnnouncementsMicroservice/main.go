5b7f31156b8290e7b92d5973190cdd683e094fd3
Akshay Elavia <akshay.elavia@sjsu.edu>
2019-11-18 19:21:38 -0800

    Added login to get count from Riak db

--- backend/AnnouncementsMicroservice/main.go
+++ backend/AnnouncementsMicroservice/main.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gorilla/mux"
 	"net/http"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
   )
 
   func pingHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,10 +13,26 @@ import (
 	json.NewEncoder(w).Encode("Announcements API is running!")
   }
 
+  func searchCount(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	response, err := http.Get("http://172.20.35.213:8098/buckets/search-counter/counters/count")
+	if err != nil {
+        fmt.Printf("The HTTP request failed with error %s\n", err)
+    } else {
+        data, _ := ioutil.ReadAll(response.Body)
+        fmt.Println(string(data))
+    }
+
+	json.NewEncoder(w).Encode("Announcements API is running!")
+  }
+
   
 func main() {
 router := mux.NewRouter()
 router.HandleFunc("/ping", pingHandler).Methods("GET")
+
+router.HandleFunc("/search-count", searchCount).Methods("GET")
 
 
 http.ListenAndServe(":8080", router)
