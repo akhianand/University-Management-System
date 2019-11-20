6dd809ba59d41d9bab9d3662d265f7c98990c019
hariae <aehari2010@gmail.com>
2019-11-19 16:09:00 -0800

    Added changes to enrollment and payment containers

--- frontend/student-portal/src/components/Enrollment/EnrollmentContainer.js
+++ frontend/student-portal/src/components/Enrollment/EnrollmentContainer.js
@@ -1,35 +1,66 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
+import { enrollmentServiceURL } from '../../config/Config';
 
 class EnrollmentContainer extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            enrollmentInfo : []
+        }
+
+        //bind
+    }
+
+    componentDidMount() {
+        let studentId = 1001;
+        axios.get(enrollmentServiceURL + '/enrollment?StudentId=' + studentId)
+        .then((res) => {
+          
+            let enrolledCourses = res.data;        
+            
+            let enrollmentInfoArray = [];
+            for(let course of enrolledCourses) {
+                let entry = new Object();
+                entry.class = course.DepartmentName + ' ' + course.CourseId;
+                entry.courseName = course.CourseName;                        
+                enrollmentInfoArray.push(entry);
+            }          
+            this.setState ({
+                enrollmentInfo : enrollmentInfoArray
+            });
+            console.log('Result of enrollment Info items: ', this.state.enrollmentInfo);
+        });
+    }
+
     render() {
+
+        let enrollmentInfo = this.state.enrollmentInfo.map((courseInfo, index) => {
+            return (
+                <tr key={index}>                                                
+                    <td>{courseInfo.class}</td>
+                    <td>{courseInfo.courseName}</td>                
+                </tr>
+            )
+            
+        });
+
         return (
             <div>
                 <div className="p-4 border courses-container">
                     <p className="section-headline">Courses</p>
                     <div>
-                        <table class="table table-bordered">
+                        <table className="table table-bordered text-align-center">
                             <thead>
                                 <tr>                                
                                 <th scope="col">Class</th>
-                                <th scope="col">Schedule</th>                                
+                                <th scope="col">Course Name</th>                                
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>                                
-                                <td>CMPE 281</td>
-                                <td>SA 9 AM to 12 PM</td>
-                                
-                                </tr>
-                                <tr>
-                                
-                                <td>CMPE 295 A</td>
-                                <td>FR 6 PM to 8:45 PM</td>                                
-                                </tr>
-                                <tr>                               
-                                <td>CMPE 294</td>
-                                <td>WE 6 PM to 8:45 PM</td>
-                                </tr>
+                                {enrollmentInfo}
                             </tbody>
                         </table>
                     </div>
--- frontend/student-portal/src/components/Payment/PaymentContainer.js
+++ frontend/student-portal/src/components/Payment/PaymentContainer.js
@@ -1,14 +1,45 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
+import {enrollmentServiceURL} from '../../config/Config';
 
 class PaymentContainer extends Component {
-    render() {
+    constructor(props) {
+        super(props);    
+        this.state = {
+            feesAmount : 0
+        }
+        //bind
+    }
+
+    componentDidMount() {
+        let studentId = 1001;
+        axios.get(enrollmentServiceURL + '/enrollment?StudentId=' + studentId)
+        .then((res) => {
+            let enrolledCourses = res.data;
+            let fees = 0;
+                
+            for (let course of enrolledCourses) {
+                if(course.HasFeesPaid === false)
+                    fees += course.Fees;
+            }
+
+            this.setState({
+                feesAmount : fees
+            });
+        })
+    }
+
+
+
+    
+    render() {        
         return (
             <div>
                 <div className="p-4 mt-5 border payment-container">
                     <p  className="section-headline">Payment</p>
                     <div>
-                        <p>You owe $5,105.00</p>
+                        <p>You owe ${this.state.feesAmount}</p>
                     </div>
                     <div>
                         <Link to="/payment/1001">Proceed to Payment >>></Link>
