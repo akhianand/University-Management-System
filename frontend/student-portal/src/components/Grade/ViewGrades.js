5b9d7d42fc4dc7bbfcf5570d468deffa9cec7bc5
Samarth Khatwani <samarthkhatwani47@gmail.com>
2019-11-20 20:30:27 -0800

    View grades ajax call added

--- frontend/student-portal/src/components/Grade/ViewGrades.js
+++ frontend/student-portal/src/components/Grade/ViewGrades.js
@@ -1,16 +1,53 @@
 import React, { Component } from 'react';
 import Header from '../Header/Header';
 import SidePane from '../SidePane/SidePane'
+import { getURL } from '../../config/Config';
+import axios from 'axios'
 
 class ViewGrades extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            grades: []
+            grades: [],
+            errorMessage : null
+        }
+        this.gradeRows = this.gradeRows.bind(this);
+    }
+    gradeRows = ()=> {
+        let grades = this.state.grades.map((gradeObj, index)=>
+              <tr className="text-align-center" key={index}>      
+                    <td>{index+1}</td>
+                    <td>{gradeObj.CourseID}</td>
+                    <td>{gradeObj.CourseName}</td>
+                    <td>{gradeObj.Term}</td>
+                    <td>{gradeObj.Grade}</td>
+              </tr>
+          );
+          return grades;
+    }
+    componentDidMount() {
+        const studentID = 430;
+        const viewGradesURL = getURL("/grades" + "?StudentID=" + studentID);
+        try {
+            axios.get(viewGradesURL)
+                .then((res) => {
+
+                    this.setState({
+                        grades: res.data
+                    });
+                    console.log('Grades : ', this.state.grades);
+                });
+        } catch (error) {
+            console.log(error);
+            console.log("Error in fetching grades!");
+            this.setState({
+                grades: [],
+                errorMessage: "Error in fetching grades!"
+            }
+            )
         }
     }
     render() {
-        var errorMessage = null
         var style = {
             height: '100vh'
         }
@@ -23,7 +60,7 @@ class ViewGrades extends Component {
                         <div className="main-container col-8 bg-white p-3">
                             <div className="container mt-5">
                                 <div className="text-align-center mb-5 enrollment-cart-heading">
-                                    <h3>Your Grades</h3>
+                                    <h3>Your Transcript</h3>
                                 </div>
                                 <table className="table table-bordered table-striped">
                                     <thead>
@@ -36,7 +73,7 @@ class ViewGrades extends Component {
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        {/* {cartItems} */}
+                                        { this.gradeRows() }
                                     </tbody>
                                 </table>
 
