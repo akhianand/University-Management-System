5f8bcc0820e96363ac52cbde45f7325ef6c90c47
hariae <aehari2010@gmail.com>
2019-11-22 10:37:31 -0800

    Updated Service URL

--- frontend/student-portal/src/config/Config.js
+++ frontend/student-portal/src/config/Config.js
@@ -7,4 +7,4 @@ export function getURL (route) {
     return baseURL + "/" +route;
 }
 
-export const enrollmentServiceURL = "http://localhost:8086";
+export const enrollmentServiceURL = "http://enrollment-nlb-047c901a12a29dbf.elb.us-west-2.amazonaws.com";
