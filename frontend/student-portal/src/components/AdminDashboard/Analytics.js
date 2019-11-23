58a40325785f90eeaeb6ee341b4e71fbf9875be6
hariae <aehari2010@gmail.com>
2019-11-23 02:31:00 -0800

    Changes to Analytics

--- frontend/student-portal/src/components/AdminDashboard/Analytics.js
+++ frontend/student-portal/src/components/AdminDashboard/Analytics.js
@@ -10,17 +10,31 @@ class Analytics extends Component {
         super(props);
         
         this.state = {
-            popularCoursedata : {
-                labels: ["2013", "2014", "2015", "2016", "2017"],
+            popularCoursedata : {},
+            bestPerformingCourses : {
+                labels: ["Cloud Computing", "Cloud Technologies", "Software Systems Engineering", "Distributed Systems", "Distributed Computing"],
                 datasets: [
                     {
-                        label: "Classification based on Year", data: [10,15,20,25,30],
+                        label: "Best performing courses", data: [3.56,3.76,3.45,3.4, 3.57],
                         backgroundColor: ['#FF6384',
                         '#36A2EB',
                         '#FFCE56','#CCC',
                         '#4BC0C0']
                     }
-                ]},
+                ]
+            },
+            worstPerformingCourses : {
+                labels: ["Cloud Computing", "Cloud Technologies", "Software Systems Engineering", "Distributed Systems",  "Distributed Computing"],
+                datasets: [
+                    {
+                        label: "Worst performing courses", data: [3.2, 2.8, 3.0, 2.97, 3.02],
+                        backgroundColor: ['#FF6384',
+                        '#36A2EB',
+                        '#FFCE56','#CCC',
+                        '#4BC0C0']
+                    }
+                ]
+            },
         }
     }
 
@@ -32,7 +46,32 @@ class Analytics extends Component {
     getPopularCoursesData = () => {
         axios.get(getURL('/click-count'))
             .then((res) => {
-                console.log('click count ', res);
+                //console.log(res.data.Response);
+                let courseData = res.data.Response;
+                courseData.sort((a,b) => parseInt(a.Clicks) > parseInt(b.Clicks) ? -1 : 1);  
+                courseData = courseData.slice(0,5);          
+                let labels = [];
+                let valueData = [];
+                for(let index in courseData) {
+                    labels.push(courseData[index].CourseName);
+                    valueData.push(courseData[index].Clicks);
+                }
+                console.log('click count ', courseData);
+                 console.log('labels ', labels);
+                 console.log('clicks ', valueData);
+                 this.setState({
+                    popularCoursedata : {
+                        labels : labels,
+                        datasets: [
+                            {
+                                label: "Popular Courses", data: valueData,
+                                backgroundColor: ['#FF6384',
+                                '#36A2EB',
+                                '#FFCE56','#CCC',
+                                '#4BC0C0']
+                            }],
+                    }
+                 })
             }) 
     }
     render() {
@@ -43,16 +82,20 @@ class Analytics extends Component {
                     <div className="text-align-center mb-5 analytics-heading mt-5">
                         <h3>Analytics Dashboard</h3>
                     </div>
-                    <div className="border mt-5">
+
+                    <div className=" mt-5 p-5">
+                        <h3 className="text-align-center analytics-heading">Popular courses</h3>
                         <Doughnut data={this.state.popularCoursedata}/>
                     </div>
                     <hr className="mt-5"/>
                     <div className="row mt-5">
-                        <div className="col-6 border">
-                            <HorizontalBar data={this.state.popularCoursedata}/>
+                        <div className="col-6 border-right">
+                        <h3 className="text-align-center analytics-heading">Best performing courses</h3>
+                            <Bar data={this.state.bestPerformingCourses}/>
                         </div>
-                        <div className="col-6 border">
-                            <Doughnut data={this.state.popularCoursedata}/>
+                        <div className="col-6">
+                        <h3 className="text-align-center analytics-heading">Worst performing courses</h3>
+                            <HorizontalBar data={this.state.worstPerformingCourses}/>
                         </div>
                     </div>
                 </div>  
