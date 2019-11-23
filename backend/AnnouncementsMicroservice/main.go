e8b4ba5172f09fca439afd959a0b2ae3e8b34a14
Akshay Elavia <akshay.elavia@sjsu.edu>
2019-11-23 07:31:19 -0800

    Added logic for average calculation

--- backend/AnnouncementsMicroservice/main.go
+++ backend/AnnouncementsMicroservice/main.go
@@ -220,6 +220,19 @@ func consumeGradeMessages() {
 				fmt.Println(err)
 			}
 
+			client := &http.Client{}
+			req, err := http.NewRequest(http.MethodPut, "http://172.20.39.6:8098/buckets/grades/keys/"+strconv.Itoa(data.Courseid), bytes.NewBuffer(msg.Value))
+			if err != nil {
+				panic(err)
+			}
+
+			req.Header.Set("Content-Type", "application/json")
+			resp, err := client.Do(req)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println("Creation of object in grades bucket with key: ", resp.StatusCode)
+
 			_, err = http.Post("http://172.20.39.6:8098/buckets/grades/counters/"+strconv.Itoa(data.Courseid), "text/plain", bytes.NewBuffer([]byte("1")))
 			if err != nil {
 				fmt.Println("Failed while incrementing counter for grade", err)
@@ -228,6 +241,10 @@ func consumeGradeMessages() {
 			gradeCountResp, err := http.Get("http://172.20.39.6:8098/buckets/grades/counters/" + strconv.Itoa(data.Courseid))
 			gradeCount, _ := ioutil.ReadAll(gradeCountResp.Body)
 			fmt.Println(string(gradeCount))
+
+
+
+
 
 			ifCourseRecorded, err := http.Get("http://172.20.39.6:8098/buckets/grades/keys/" + strconv.Itoa(data.Courseid))
 			if err != nil {
